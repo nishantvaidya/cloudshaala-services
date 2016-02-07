a42dabe667d3be9ed767f7db9c04c888b212d5d1
nishant <nishant.mca2005@gmail.com>
2016-02-07 10:12:23 +0530

    User Service Rest service

---
+++ initial/src/main/java/com/cloudshaala/user/bean/UserBean.java
@@ -0,0 +1,50 @@
+package com.cloudshaala.user.bean;
+
+public class UserBean {
+	private String type;
+	private String email;
+	private String mobile;
+	private String firstName;
+	private String lastName;
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+}
